99cc9473ce01edf3ce64ca39e5fd69a13e74e211
Charlos Purba <charlospurba048@gmail.com>
2024-07-01 16:19:54 +0700

    Initial commiit

---
+++ Space Adventure/MainMenuViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainMenuViewController.swift
+//  Space Adventure
+//
+//  Created by Foundation-005 on 26/06/24.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
